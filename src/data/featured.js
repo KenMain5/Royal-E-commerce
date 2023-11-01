9d01ed0e1a0d20fd5002d2f6459681dd2a3cf35b
KenMain5 <kennyize15@gmail.com>
2023-11-01 12:36:52 -0400

    fixed bug where images arent showing

--- src/data/featured.js
+++ src/data/featured.js
@@ -1,49 +1,41 @@
+import photo1 from '/src/assets/Featured2/fox1/pic1.png'
+import photo2 from '/src/assets/Featured2/fox1/pic2.png'
+import photo3 from '/src/assets/Featured2/fox1/pic3.png'
+import photo4 from '/src/assets/Featured2/fox1/pic4.png'
+import photo5 from '/src/assets/Featured2/fox1/pic5.png'
+import photo6 from '/src/assets/Featured2/fox1/pic6.png'
+import photo7 from '/src/assets/Featured2/fox1/pic7.png'
+import photo8 from '/src/assets/Featured2/fox1/pic8.png'
+import photo9 from '/src/assets/Featured2/tiee/bagss1.png'
+import photo10 from  '/src/assets/Featured2/tiee/bagss2.png'
+import photo11 from  '/src/assets/Featured2/tiee/bagss3.png'
+import photo12 from  '/src/assets/Featured2/tiee/tiee1.png'
+import photo13 from  '/src/assets/Featured2/tiee/tiee2.png'
+import photo14 from  '/src/assets/Featured2/tiee/tiee3.png'
+import photo15 from  '/src/assets/Featured2/tiee/tiee5.png'
+import photo16 from  '/src/assets/Featured2/tiee/tiee6.png'
+import photo17 from  '/src/assets/Featured2/tiee/tiee7.png'
+import photo18 from  '/src/assets/Featured2/tiee/tiee8.png'
+
 const FEATURED = [
-    {'id': 1, 'price': '$980', 'url': '/src/assets/Featured2/fox1/pic1.png', 'name': "Raphael Exclusive", 'category': 'clothing'},
-    {'id': 2, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic2.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 3, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic3.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 4, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic4.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 5, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic5.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 6, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic6.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 7, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic7.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 8, 'price': '$230', 'url': '/src/assets/Featured2/fox1/pic8.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 9, 'price': '$230', 'url': '/src/assets/Featured2/tiee/bagss1.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 10, 'price': '$230', 'url': '/src/assets/Featured2/tiee/bagss2.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 11, 'price': '$230', 'url': '/src/assets/Featured2/tiee/bagss3.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 12, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee1.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 13, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee2.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 14, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee3.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 15, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee5.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-
-    {'id': 16, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee6.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 17, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee7.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-    {'id': 18, 'price': '$230', 'url': '/src/assets/Featured2/tiee/tiee8.png', 'name': "Raphael Exclusive", 'category': 'bag'},
-
-
-
-
-
+    {'id': 1, 'price': '$980', 'url': photo1, 'name': "Raphael Exclusive", 'category': 'clothing'},
+    {'id': 2, 'price': '$230', 'url': photo2, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 3, 'price': '$230', 'url': photo3, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 4, 'price': '$230', 'url': photo4, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 5, 'price': '$230', 'url': photo5, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 6, 'price': '$230', 'url': photo6, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 7, 'price': '$230', 'url': photo7, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 8, 'price': '$230', 'url': photo8, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 9, 'price': '$230', 'url': photo9, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 10, 'price': '$230', 'url': photo10, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 11, 'price': '$230', 'url': photo11, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 12, 'price': '$230', 'url': photo12, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 13, 'price': '$230', 'url': photo13, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 14, 'price': '$230', 'url': photo14, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 15, 'price': '$230', 'url': photo15, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 16, 'price': '$230', 'url': photo16, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 17, 'price': '$230', 'url': photo17, 'name': "Raphael Exclusive", 'category': 'bag'},
+    {'id': 18, 'price': '$230', 'url': photo18, 'name': "Raphael Exclusive", 'category': 'bag'},
 ]
-/*
-const ITEMS = [
-   
-cae
-]
-
-
-//useContext ..
-//pocketBase.. to athenticate..
-//send password 
-
-
-
-
-
-*/
-
-    
-//category
-//price
-
 
 export default FEATURED
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,37 +1,20 @@
 import React from 'react'
 import './Hero.scss'
-
+import heroPicture from '/src/assets/AdobeStock_268135380.jpeg'
 
 function Hero() {
   return (
     <section className='hero'>
         <div className='hero__contents'>
-          <img src="/src/assets/AdobeStock_268135380.jpeg" alt="" />
+          <img src={heroPicture} alt="picture of a model in a classy expensive suit" />
           <div className='hero__contents-text'>
             <span>the names to know in menswear</span>
             <span>A series of exclusive collections, curated by Raphaël Clemont</span>
             <span>Shop the collection</span>
           </div>
         </div>
-       
-       
     </section>
   )
 }
 
 export default Hero
-
-
-{/* <div className='hero__text'>
-            <div className='hero__text-header'>
-              <h1>Brucello Cucinelli</h1>
-            </div>
-
-            <div className='hero__text-desc'>  
-              <p>Turn to Brunello Cucinelli for menswear that whispers rather than shouts. With a reputation for</p>
-              {/* <p>Turn to Brunello Cucinelli for menswear that whispers rather than shouts. With a reputation for sumptuous fabrications, detail-focused Italian design and minimal branding, the house’s name has become something of a byword for stealth luxury. The brand makes a strong case for ignoring trends, instead favoring all things timeless. It’s a winning formula—these are pieces to hold on to year after year and, thanks to their impeccable craftsmanship, they'll easily stand the test of time.</p> */}
-        //     </div>
-
-        // </div>
-        
-        // <div className='hero__image'><img src='/src/assets/AdobeStock_268135380.jpeg'></img></div> */}
